411b99759cba13c61b056baa8a700c44eddc4a94
JohnChen <johnchen@@apple.com>
2022-09-13 19:15:05 -0700

    Initial Commit

---
+++ src/helloworld.c
@@ -0,0 +1,15 @@
+// Simple C program to display "Hello World"
+  
+// Header file for input output functions
+#include <stdio.h>
+  
+// main function -
+// where the execution of program begins
+void  helloworld()
+{
+  
+    // prints hello world
+    printf("Hello World");
+  
+    return 0;
+}
